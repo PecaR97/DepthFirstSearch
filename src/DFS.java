1e6955d0227af4b8ada6766e23bcb39c8cd97806
PecaR97 <62112381+PecaR97@users.noreply.github.com>
2021-03-13 12:44:09 +0100

    Version 1.1

--- src/DFS.java
+++ src/DFS.java
@@ -16,33 +16,83 @@ public class DFS {
         this.isFound = false;
     }
 
-    public void setSource(Node source){
+    public void setSource(Node source) {
         this.source = source;
     }
 
-    public void reset(){
+    public void reset() {
         this.openedNodes = new ArrayList<>();
         this.priorityList = new ArrayList<>();
         this.isFound = false;
     }
 
-    public void reset(String source){
+    public void reset(String source) {
         reset();
         this.source = new Node(source, null);
     }
 
-    public void search(String destination){
+    public void search(String destination) {
         this.priorityList.add(source);
+        this.openedNodes.add(source);
 
-        while(!priorityList.isEmpty()){
+        while (!priorityList.isEmpty()) {
             Node currentNode = priorityList.get(0);
-            if(currentNode.getValue() == destination){
+            if (currentNode.getValue() == destination) {
                 this.isFound = true;
                 break;
             } else {
-
+                openChildren(currentNode);
+                this.priorityList.remove(0);
+                addChildrenToPriorityList(currentNode);
             }
         }
+        System.out.println(isFound ? "Found" : "Not found");
+        System.out.println();
+        printOpenedNodes();
 
     }
+
+    private void addChildrenToPriorityList(Node currentNode) {
+        ArrayList<Node> newList = new ArrayList<>();
+        if (currentNode.getChildren().isEmpty()) {
+            return;
+        } else {
+            for (Node child : currentNode.getChildren()) {
+                newList.add(child);
+            }
+            newList.addAll(this.priorityList);
+            this.priorityList = newList;
+        }
+    }
+
+    public void openChildren(Node currentNode) {
+        String[] listOfChildren = table.get(currentNode.getValue());
+        if (listOfChildren.length == 0) {
+            return;
+        }
+        for (int i = 0; i < listOfChildren.length; i++) {
+            if (!isOnPathToRoot(listOfChildren[i],currentNode)) {
+                Node newNode = new Node(listOfChildren[i], currentNode);
+                openedNodes.add(newNode);
+            }
+
+        }
+    }
+
+    private boolean isOnPathToRoot(String nodeValue,Node parent) {
+        while (parent != null) {
+            if (parent.getValue() == nodeValue) {
+                return true;
+            }
+            parent = parent.getParent();
+        }
+        return false;
+    }
+
+    public void printOpenedNodes() {
+        for (int i = 0; i < openedNodes.size(); i++) {
+            System.out.printf("%-4s%s%n",(i+1)+".",this.openedNodes.get(i).getValue());
+        }
+    }
+
 }
--- src/Test.java
+++ src/Test.java
@@ -1,15 +1,19 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Test {
     public static void main(String[] args) {
-        Node A = new Node("A");
-        Node B = new Node("B",A);
-        Node C = new Node("C",A);
-        Node D = new Node("D",C);
-
-
-        System.out.println(A);
-        System.out.println(B);
-        System.out.println(C);
-        System.out.println(D);
+        HashMap<String, String[]> table = new HashMap<>();
+        table.put("A", new String[]{"B", "D", "S"});
+        table.put("B", new String[]{"A", "C", "D"});
+        table.put("C", new String[]{"B"});
+        table.put("D", new String[]{"A", "E", "S"});
+        table.put("E", new String[]{"D", "F"});
+        table.put("F", new String[]{"E", "G"});
+        table.put("G", new String[]{"F"});
+        table.put("S", new String[]{"A", "D"});
+        DFS dfs = new DFS(table,"S");
+        dfs.search("G");
 
     }
 }
